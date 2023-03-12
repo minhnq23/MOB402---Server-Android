620adc09a6a62a59a3d1f73385fefa2ec26fadbc
admin <minhnqph25450@fpt.edu.vn>
2023-03-12 18:35:44 +0700

    Btvn3

--- MyfirstNodeJs.js
+++ MyfirstNodeJs.js
@@ -3,7 +3,15 @@ var uc = require("upper-case");
 var fs = require("fs");
 const express = require("express");
 const app = express();
+var dt = require("./uniTest");
+var caculator = require("./Caculator");
+const { type } = require("os");
 
+function showInfo(mes) {
+  if (typeof mes === "string") {
+    console.log(mes.toUpperCase());
+  }
+}
 http
   .createServer(function (req, res) {
     // res.writeHead(200, { "Content-Type": "text/plain" });
@@ -15,12 +23,12 @@ http
       if (err) throw err;
       console.log("File Renamed!");
     });
-    fs.readFile("test.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      console.log("start file");
-      return res.end();
-    });
+    // fs.readFile("test.html", function (err, data) {
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.write(data);
+    //   console.log("start file");
+    //   return res.end();
+    // });
 
     // fs.unlink("del.txt", function (err) {
     //   if (err) throw err;
@@ -30,5 +38,12 @@ http
     // app.get("/", function (request, response) {
     //   response.sendFile("D:studyServer\node Untitled-1.html");
     // });
+    // showInfo("hihi");
+    // showInfo(76);
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello, world!\n");
+    res.write(uc.upperCase(`Thoi gian hien tai: ` + dt.myDateTime() + " "));
+    res.write(uc.upperCase(`\n Tong: ` + caculator.AddNumber(5, 5)));
+    res.end();
   })
   .listen(8080);
---
+++ uniTest.js
@@ -0,0 +1,7 @@
+exports.myDateTime = function () {
+  return Date();
+};
+
+exports.sum = (a, b) => {
+  return a + b;
+};
